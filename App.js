18e0139fb05c30781602af95af2dc5bd9d8d3a09
Suramya Suresh Babu <116120476+suramya98@users.noreply.github.com>
2024-02-07 13:18:30 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import PeerEditor from './components/PeerEditor';
+
+class App extends Component {
+    render() {
+      return (
+        <div className="App">
+          <main>
+            <PeerEditor />
+          </main>
+        </div>
+      );
+    }
+  }
+  
+  export default App;
+  
--- index.js
+++ index.js
@@ -1,45 +1,21 @@
-import './index.scss';
-import osjs from 'osjs';
-import {name as applicationName} from './metadata.json';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/App';
 
-// Our launcher
-const register = (core, args, options, metadata) => {
-  // Create a new Application instance
-  const proc = core.make('osjs/application', {args, options, metadata});
+module.exports = function(props) {
+  return (
+  <div>
+    <h1>MULE</h1>
 
-  core.make('client/log/api').log('Hello from PeerProgram App');
+    <div>
+      Created by Kevin Casey, Natalie Culligan, Katie Brugha, Ruiqi Li, Vanush "Misha" Paturyan
+    </div>
 
-  // Create  a new Window instance
-  const window1 = proc.createWindow({
-    id: 'peerProgramWindow',
-    title: metadata.title.en_EN,
-    dimension: {width: 600, height: 600},
-    position: {left: 700, top: 200}
-  })
+    <div>
+      <img alt="" src="about.png" />
+    </div>
 
-  
+    <div>
 
-  window1.on('destroy', () => proc.destroy())
-    .render($content => ReactDOM.render(React.createElement(App,{proc:proc}), $content));
-
-  // Creates a new WebSocket connection (see server.js)
-  //const sock = proc.socket('/socket');
-  //sock.on('message', (...args) => console.log(args))
-  //sock.on('open', () => sock.send('Ping'));
-
-  // Use the internally core bound websocket
-  //proc.on('ws:message', (...args) => console.log(args))
-  //proc.send('Ping')
-
-  // Creates a HTTP call (see server.js)
-  //proc.request('/test', {method: 'post'})
-  //.then(response => console.log(response));
-
-  return proc;
+    </div>
+  </div>
+);
 };
-
-// Creates the internal callback function when OS.js launches an application
-osjs.register(applicationName, register);
